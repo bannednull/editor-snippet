d2cfc4238dabbbfef943ece18ff2949f0d6de3c8
jsstoni <jsstoniha@gmail.com>
2024-09-22 23:48:40 -0300

    agregando rutas

---
+++ src/components/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="text-7xl font-bold">404</h1>
+      <h2 className="text-3xl">Page not found</h2>
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import NotFound from "./components/not-found.tsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: ":slug",
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
