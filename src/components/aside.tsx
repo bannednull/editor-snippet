cebf650b64c7d1d040800830cb07830204b84497
jsstoni <jsstoniha@gmail.com>
2024-09-25 13:35:40 -0300

    logo

--- src/components/aside.tsx
+++ src/components/aside.tsx
@@ -2,9 +2,9 @@ import DialogNew from "./dialog-new";
 
 export default function Aside() {
   return (
-    <aside className="flex flex-col gap-2 border-r px-3 py-2 bg-muted/40">
+    <aside className="flex flex-col gap-2 border-r p-3 bg-muted/40">
       <div className="flex items-center">
-        <img src="/icon.svg" alt="logo" className="size-8" />
+        <img src="/logo.svg" alt="logo" className="size-7" />
         <p className="ml-2">Bakan</p>
 
         <DialogNew />
