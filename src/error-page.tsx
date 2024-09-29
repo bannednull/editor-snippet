e38daa4709aa7009a4c0f969916e63075012dc8a
jsstoni <jsstoniha@gmail.com>
2024-09-29 18:10:13 -0300

    mensaje de error

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -8,7 +8,10 @@ export default function ErrorPage() {
       <h1 className="text-4xl">Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>
+          {(error as Error)?.message ||
+            (error as { statusText?: string })?.statusText}
+        </i>
       </p>
     </div>
   );
