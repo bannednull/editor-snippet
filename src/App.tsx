4ec2bbaf5377f94c4c4cab8809b6a8186ce0fb80
jsstoni <jsstoniha@gmail.com>
2024-09-29 18:09:31 -0300

    layout app

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,24 @@
-import { ThemeProvider } from "@/components/theme-provider";
-import Aside from "./components/aside";
-import Capture from "./components/capture";
-import CodeEditor from "./components/code-editor";
-import CodeSuggestion from "./components/code-suggestion";
-import ComboLang from "./components/combo-lang";
-import LineColumn from "./components/line-column";
-import SaveSnippet from "./components/save-snippet";
-import SessionProvider from "./components/session-provider";
-import { Toaster } from "./components/ui/toaster";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import type { Snippet } from "./api/snippet";
+import { createSnippetStore } from "./stores/snippets";
 
-function App() {
+const LazyCodeEditor = React.lazy(() => import("./components/code-editor"));
+
+export default function App() {
+  const snippet = useLoaderData() as Snippet;
+
+  const setSnippet = createSnippetStore((state) => state.setSnippet);
+
+  React.useEffect(() => {
+    if (snippet) {
+      setSnippet(snippet);
+    }
+  }, [snippet, setSnippet]);
+
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <SessionProvider>
-        <div className="grid h-screen md:grid-cols-[300px,1fr]">
-          <Aside />
-          <CodeSuggestion />
-
-          <div className="relative flex flex-col h-screen overflow-hidden">
-            <div className="flex-grow overflow-auto">
-              <CodeEditor />
-            </div>
-
-            <div className="bg-black/40 py-1 text-muted-foreground border-t flex items-center text-xs px-2">
-              <Capture />
-              <SaveSnippet />
-
-              <div className="ml-auto flex gap-3 items-center">
-                <LineColumn />
-                <ComboLang />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <Toaster />
-      </SessionProvider>
-    </ThemeProvider>
+    <React.Suspense fallback={<div>Loading...</div>}>
+      <LazyCodeEditor />
+    </React.Suspense>
   );
 }
-
-export default App;
