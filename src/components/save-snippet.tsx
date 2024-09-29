dea8a39b1eca92ffef42fde50cd7ceb7956f6789
jsstoni <jsstoniha@gmail.com>
2024-09-29 18:10:35 -0300

    visualizar el titulo

--- src/components/save-snippet.tsx
+++ src/components/save-snippet.tsx
@@ -1,8 +1,14 @@
 import { useFetcherWithReset } from "@/hooks/useUtils";
 import { createSnippetStore } from "@/stores/snippets";
 import React from "react";
+import { useShallow } from "zustand/shallow";
 
 export default function SaveSnippet() {
+  console.log("TITLE SNIPPET");
+  const { title } = createSnippetStore(
+    useShallow((state) => ({ title: state.title })),
+  );
+
   const fetcher = useFetcherWithReset<{ error: string }>();
 
   const onChange = React.useCallback(
@@ -23,6 +29,7 @@ export default function SaveSnippet() {
         type="text"
         className="bg-transparent py-1 px-3 border rounded-md w-[270px]"
         placeholder="describe your snippet..."
+        value={title}
         onChange={onChange}
       />
       <button
