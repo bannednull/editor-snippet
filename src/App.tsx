9d6b91f24daf8e31c5bd6740a2601cc830c9798e
jsstoni <jsstoniha@gmail.com>
2024-10-03 01:56:19 -0300

    moviendo componentes app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 import type { Snippet } from "./api/snippet";
 import { CodeLoader } from "./components/code-loader";
+import CodeSuggestion from "./components/code-suggestion";
+import ComboLang from "./components/combo-lang";
+import LineColumn from "./components/line-column";
 import NotFound from "./components/not-found";
+import SaveSnippet from "./components/save-snippet";
 import { createSnippetStore } from "./stores/snippets";
 
 const LazyCodeEditor = React.lazy(() => import("./components/code-editor"));
@@ -27,10 +31,21 @@ export default function App() {
   }, [snippet, setSnippet]);
 
   return (
-    <div className="flex-grow overflow-auto">
-      <React.Suspense fallback={<CodeLoader />}>
-        <LazyCodeEditor value={snippet?.code ?? ""} />
-      </React.Suspense>
-    </div>
+    <>
+      <SaveSnippet />
+
+      <div className="flex-grow overflow-auto">
+        <React.Suspense fallback={<CodeLoader />}>
+          <LazyCodeEditor value={snippet?.code ?? ""} />
+        </React.Suspense>
+      </div>
+
+      <div className="bg-black/40 text-muted-foreground border-t flex items-center gap-2 text-xs px-2">
+        <LineColumn />
+        <ComboLang />
+      </div>
+
+      <CodeSuggestion />
+    </>
   );
 }
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,10 +1,6 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Outlet } from "react-router-dom";
 import Aside from "./components/aside";
-import CodeSuggestion from "./components/code-suggestion";
-import ComboLang from "./components/combo-lang";
-import LineColumn from "./components/line-column";
-import SaveSnippet from "./components/save-snippet";
 import SessionProvider from "./components/session-provider";
 import { Toaster } from "./components/ui/toaster";
 
@@ -14,17 +10,9 @@ function Layout() {
       <SessionProvider>
         <div className="grid h-screen md:grid-cols-[300px,1fr]">
           <Aside />
-          <CodeSuggestion />
 
           <div className="relative flex flex-col h-screen overflow-hidden">
-            <SaveSnippet />
-
             <Outlet />
-
-            <div className="bg-black/40 text-muted-foreground border-t flex items-center gap-2 text-xs px-2">
-              <LineColumn />
-              <ComboLang />
-            </div>
           </div>
         </div>
 
