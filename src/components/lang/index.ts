5731d292723f3ce5154891a58025990125834ff1
jsstoni <jsstoniha@gmail.com>
2024-10-07 15:24:37 -0300

    lenguajes soportados

--- src/components/lang/index.ts
+++ src/components/lang/index.ts
@@ -1,40 +1,12 @@
 import type { FC } from "react";
 
-import c from "./c";
-import cpp from "./cpp";
-import csharp from "./csharp";
-import css from "./css";
-import dart from "./dart";
-import go from "./go";
 import html from "./html";
-import java from "./java";
 import javascript from "./javascript";
-import kotlin from "./kotlin";
-import markdown from "./markdown";
-import php from "./php";
-import python from "./python";
-import ruby from "./ruby";
-import rust from "./rust";
-import sql from "./sql";
 import typescript from "./typescript";
 
 const iconLang: { [key: string]: FC } = {
-  c: c,
-  cpp: cpp,
-  csharp: csharp,
-  css,
-  dart,
-  go,
   html,
-  java,
   javascript,
-  kotlin,
-  markdown,
-  php,
-  python,
-  ruby,
-  rust,
-  sql,
   typescript,
 };
 
